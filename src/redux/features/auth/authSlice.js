af7de1d27be8d07540aeeb7a1400b4d6b4646ac3
Gosa D Dadi <57652085+gosadadi@users.noreply.github.com>
2022-12-01 09:30:35 -0800

    Update authSlice.js

--- src/redux/features/auth/authSlice.js
+++ src/redux/features/auth/authSlice.js
@@ -1,44 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
-const name = JSON.parse(localStorage.getItem("name")) || null
+const name = JSON.parse(localStorage.getItem("name")) || null;
+
 const initialState = {
-    isLoggedIn: false,
-    name:name ? name : "",
-    user: {
-        name: "",
-        email:"",
-        bio:"",
-        photo:"",
-        phone:""
-    }
+  isLoggedIn: false,
+  name: name ? name : "",
+  user: {
+    name: "",
+    email: "",
+    phone: "",
+    bio: "",
+    photo: "",
+  },
 };
 
 const authSlice = createSlice({
-    name: "auth",
-    initialState,
-    reducers: {
-        SET_LOGIN(state, action){
-            state.isLoggedIn = action.payload
-            localStorage.setItem("name", JSON.stringify(action.payload))
-        },
-        SET_NAME(state, action){
-            state.name = action.payload
-        },
-        SET_USER(state, action){
-            const profile = action.payload;
-            state.user.name = profile.name;
-            state.user.email = profile.email;
-            state.user.phone = profile.phone;
-            state.user.photo = profile.photo;
-            state.user.bio = profile.bio;
-        },
-
+  name: "auth",
+  initialState,
+  reducers: {
+    SET_LOGIN(state, action) {
+      state.isLoggedIn = action.payload;
     },
+    SET_NAME(state, action) {
+      localStorage.setItem("name", JSON.stringify(action.payload));
+      state.name = action.payload;
+    },
+    SET_USER(state, action) {
+      const profile = action.payload;
+      state.user.name = profile.name;
+      state.user.email = profile.email;
+      state.user.phone = profile.phone;
+      state.user.bio = profile.bio;
+      state.user.photo = profile.photo;
+    },
+  },
 });
 
-export const selectIsLoggedIn = (state) => state.auth.isLoggedIn
-export const { SET_LOGIN, SET_NAME, SET_USER} = authSlice.actions;
-export const selectName = (state) => state.auth.name
-export const selectUser = (state) => state.auth.user
+export const { SET_LOGIN, SET_NAME, SET_USER } = authSlice.actions;
 
-export default authSlice.reducer
+export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
+export const selectName = (state) => state.auth.name;
+export const selectUser = (state) => state.auth.user;
+
+export default authSlice.reducer;
